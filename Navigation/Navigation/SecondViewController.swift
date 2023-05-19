3fc5b266d0e94e53639a93eed26b363d4a817665
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-05-19 16:34:28 +0200

    Third Commit

--- Navigation/Navigation/SecondViewController.swift
+++ Navigation/Navigation/SecondViewController.swift
@@ -16,14 +16,9 @@ class SecondViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnPressed(_ sender: UIButton) {
+        let storyBoard = self.storyboard?.instantiateViewController(withIdentifier: "ThirdViewController") as? ThirdViewController
+        //self.navigationController?.pushViewController(storyBoard, animated: true)
     }
-    */
-
+    
 }
